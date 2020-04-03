9eb5c2a846f7986cd95b1f5ef6146d87bce5355f
ivandevp <medinadiazivan@gmail.com>
2020-04-03 07:31:50 -0600

    Paso 5: agrega rutas de login y teams

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: 'recommended',
   rules: {
     'no-html-comments': false,
-    'no-implicit-this': { allow: ['current-time'] },
+    'no-implicit-this': { allow: ['format-timestamp', 'current-time'] },
     'img-alt-attributes': false,
     'self-closing-void-elements': false,
     quotes: false
