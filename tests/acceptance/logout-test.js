715cb0b7e275711cd69833d7e40ced1383e9851d
ivandevp <medinadiazivan@gmail.com>
2020-04-04 13:35:51 -0600

    Paso 10: corrige tests de servicio de autenticación

--- tests/acceptance/logout-test.js
+++ tests/acceptance/logout-test.js
@@ -1,13 +1,18 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import MockAuthService from '../stubs/auth-service';
 
 module('Acceptance | logout', function(hooks) {
   setupApplicationTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.owner.register('service:auth', MockAuthService);
+  })
+
   test('visiting /teams', async function(assert) {
     await visit('/teams');
-
+    await this.pauseTest();
     assert.equal(currentURL(), '/teams');
 
     await click('.team-sidebar__logout-button');
---
+++ tests/stubs/auth-service.js
@@ -0,0 +1,15 @@
+import Service, { inject as service } from '@ember/service';
+import Router from '@ember/routing/router';
+
+export default class MockAuthService extends Service {
+  currentUserId = null;
+  /**
+   * @type {Router}
+   */
+  @service router;
+
+  loginWithUserId(userId) {
+    this.currentUserId = userId;
+    this.router.transitionTo('teams');
+  }
+}
